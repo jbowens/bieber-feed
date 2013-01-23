9d0c26da26f6b13b5db04869ce7c07b1b404a750
Nick Jacob <njacob@groupcommerce.com>
2013-01-23 13:50:14 -0500

    making bieber client better/safer; throws custom errors all the time now

--- bieber.client.js
+++ bieber.client.js
@@ -5,145 +5,214 @@
  * @author nbjacob
  * Spring 2013
  ***/
-;(function __CS132__(window, undefined) {
-  var BIEBER_URL = 'http://localhost:1337/feed/{{login}}',
-      ERROR_THRESHOLD = 100, err_count = 0, loop = null, callbacks = {},
-      Bieber = window.Bieber = window.Bieber || {},
-      received = {}; // ID => Tweet
+;(function (window, undefined) {
 
+  function __CS132__(window, url, login, undefined) {
 
-  function ajax (conf) {
-    var request = new XMLHttpRequest(),
-        params = parametize(conf.data);
+    var BIEBER_URL = url + '/' + login;
+        ERROR_THRESHOLD = 100, err_count = 0, loop = null, callbacks = {},
+        errorHandler = null,
+        Bieber = window.Bieber = window.Bieber || {},
+        // list the events to give
+        hasOwnProperty = Object.prototype.hasOwnProperty,
+        toString = Object.prototype.toString,
+        noop = function() {},
+        EVENTS = {
+          tweet: 'tweet',
+          start: 'start',
+          stop: 'end',
+          end: 'end'
+        },
+        received = {}; // ID => Tweet
 
-    conf.method = conf.method ? conf.method.toUpperCase() : 'GET';
-    conf.url += (conf.method === 'GET') ? '?' + params : '';
-    request.open(conf.method, conf.url, true);
-
-    if (conf.method === 'POST') {
-      request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-      request.setRequestHeader('Content-length', params.length);
-      request.setRequestHeader('Connection', 'close');
-      request.send(params);
-    } else {
-      request.send();
+    // custom error
+    function BieberFeedError(msg) {
+      this.name = 'BieberFeedError';
+      Error.call(this, msg);
     }
 
-    request.onreadystatechange = function () {
-      if (request.readyState === 4) {
-        if (request.status === 200) {
-          conf.success(request.responseText, request);
-        } else {
-          conf.error(request);
+    function __err__() {};
+    __err__.prototype = Error.prototype;
+    BieberFeedError.prototype = new __err__();
+    BieberFeedError.prototype.constructor = BieberFeedError;
+
+    // helpers
+    function isFunction(fn, arity) {
+      ((toString.call(fn) === '[object Function]') &&
+      (arity !== undefined && fn.length >= arity));
+    }
+
+
+    function ajax (conf) {
+      var request = new XMLHttpRequest(),
+          params = parametize(conf.data);
+
+      conf.method = conf.method ? conf.method.toUpperCase() : 'GET';
+      conf.url += (conf.method === 'GET') ? '?' + params : '';
+      request.open(conf.method, conf.url, true);
+
+      if (conf.method === 'POST') {
+        request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        request.setRequestHeader('Content-length', params.length);
+        request.setRequestHeader('Connection', 'close');
+        request.send(params);
+      } else {
+        request.send();
+      }
+
+      request.onreadystatechange = function () {
+        if (request.readyState === 4) {
+          if (request.status === 200) {
+            conf.success(request.responseText, request);
+          } else {
+            conf.error(request);
+          }
         }
       }
     }
-  }
 
-  function parametize(data) {
-    if (!data) return null;
+    function parametize(data) {
+      if (!data) return null;
 
-    var params = '', key;
-    for(key in data) {
-      if (Object.prototype.hasOwnProperty.call(data,key)) {
-        params += encodeURIComponent(key) + '=' + encodeURIComponent(data[key]) + '&';
+      var params = '', key;
+      for(key in data) {
+        if (hasOwnProperty.call(data,key)) {
+          params += encodeURIComponent(key) + '=' + encodeURIComponent(data[key]) + '&';
+        }
+      }
+      return params;
+    }
+
+    // create custom getters & setters for the events 'on' + event
+    for(var evt in EVENTS) {
+      if (hasOwnProperty.call(EVENTS,evt)) {
+        (function(event) {
+          Object.defineProperty(Bieber, 'on' + event, {
+            set: function (val) {
+              if (!isFunction(val,1)) {
+                throw new BieberFeedError("Event listener must be a function that takes one argument. Found: " + toString.call(val) + " | takes: " + val.length + " arguments");
+              }
+              else {
+                Bieber.addEventListener(event, val);
+              }
+            },
+            get: function() {
+            }
+
+          });
+        }(evt));
+
       }
     }
-    return params;
-  }
 
-  //
-  // Bieber.ontweet
-  // this sets a callback for the 'tweet' event, a faux-event that contains the data from a new tweet received from the server
-  //
-  Bieber.ontweet = null;
 
-  //
-  // Bieber.onerror
-  //
-  Bieber.onerror = null;
+    // Bieber.start()
+    // begin polling the server
+    // @emits a start event
+    Bieber.start = function (interval) {
+      setTimeout(function() {
+        interval = interval*1000 || 1000;
+        loop = setInterval(_callAPI, interval);
+        _emit('start');
+      }, 10);
+    };
 
-  // Bieber.start()
-  // begin polling the server
-  // @emits a start event
-  Bieber.start = function (interval) {
-    setTimeout(function() {
-      interval = interval*1000 || 1000;
-      loop = setInterval(_callAPI, interval);
-      _emit('start');
-    }, 10);
-  };
+    // Bieber.stop()
+    // stop the polling :)
+    // @emits a stop event
+    Bieber.stop = function () {
+      if (loop) {
+        clearInterval(loop);
+        _emit('end');
+      }
+    };
 
-  // Bieber.stop()
-  // stop the polling :)
-  // @emits a stop event
-  Bieber.stop = function () {
-    if (loop) {
-      clearInterval(loop);
-      _emit('end');
+    // Bieber.addEventListener
+    // add a listener for the given event
+    Bieber.addEventListener = function (event, cb) {
+      if (EVENTS[event] && (event = EVENTS[event])) {
+        callbacks[event] = callbacks[event] || [];
+        callbacks[event].push(cb);
+
+        if (event === 'error') {
+          errorHandler = cb;
+        }
+
+        return callbacks[event].length;
+      } else {
+        throw new BieberFeedError("attempt to assign listener for nonexistent event: " + event);
+      }
     }
-  };
 
-  // Bieber.addEventListener
-  // add a listener for the given event
-  Bieber.addEventListener = function (event, cb) {
-    callbacks[event] = callbacks[event] || [];
-    callbacks[event].push(cb);
-  }
+    // Bieber.done
+    // made for people who are trying to do a node-esque API
+    Bieber.done = function (cb) {
+      Bieber.addEventListener('tweet', function (tweet) {
+        cb(null, tweet);
+      });
 
-  // Bieber.done
-  // made for people who are trying to do a node-esque API
-  Bieber.done = function (cb) {
-    Bieber.addEventListener('tweet', function (tweet) {
-      cb(null, tweet);
-    });
-
-    Bieber.addEventListener('error', function (err) {
-      cb(err, null);
-    });
-  }
+      Bieber.addEventListener('error', function (err) {
+        cb(err, null);
+      });
+    }
 
 
-  /// PRIVATE FUNCTIONS
+    /// PRIVATE FUNCTIONS
 
-  _callAPI = function() {
-    ajax({
-      method: 'GET',
-      url: BIEBER_URL,
-      success: function (tweets) {
-        if (tweets && tweets.length) {
-          var i = 0, len = tweets.length;
-          for(; i < len; i++) {
-            var tweet = tweets[i];
-            if (tweet['id']) {
-              if (!received[tweet['id']] && (received[tweet['id']] = tweet)) {
-                _emit('tweet', tweets[i]);
+    function _callAPI() {
+      ajax({
+        method: 'GET',
+        url: BIEBER_URL,
+        success: function (tweets) {
+          if (tweets && tweets.length) {
+            var i = 0, len = tweets.length;
+            for(; i < len; i++) {
+              var tweet = tweets[i];
+              if (tweet['id']) {
+                if (!received[tweet['id']] && (received[tweet['id']] = tweet)) {
+                  _emit('tweet', tweets[i]);
+                }
               }
             }
           }
+        },
+        error: function (err) {
+          _emit('error', tweets[i]);
+          if (++err_count > ERROR_THRESHOLD) {
+            Bieber.stop(err);
+          }
         }
-      },
-      error: function (err) {
-        _emit('error', tweets[i]);
-        if (++err_count > ERROR_THRESHOLD) {
-          Bieber.stop(err);
-        }
-      }
-    });
-  }
+      });
+    }
 
-  _emit = function (event, data) {
-    if (callbacks[event] && callbacks[event].length) {
-      var cbs = callbacks[event], i = 0, len = cbs.length;
-      for(; i < len; i++) {
-        cbs(data);
+    function _emit(event, data) {
+      // if you don't have a handler for this, freak out
+      if (event === 'error' && errorHandler === null) {
+        throw new BieberFeedError("Unhandled Bieber Error: " + data);
+      }
+
+      if (callbacks[event] && callbacks[event].length) {
+        var cbs = callbacks[event], i = 0, len = cbs.length;
+        for(; i < len; i++) {
+          cbs(data);
+        }
       }
     }
 
-    var str = 'on' + event,
-        extra = Bieber[str] != null && typeof Bieber[str] === 'function' ? Bieber[str] : function(){};
-
-    extra.call(extra, data);
   }
+
+  // check that the window is ok
+  if (!window || !window.document) {
+    throw new Error("Bieber Client is not loaded in the correct context.");
+  }
+
+  else if ( !window.BIEBER_URL || !window.STUDENT_LOGIN ) {
+    throw new Error("Missing Bieber Configuration: window.BIEBER_URL and window.STUDENT_LOGIN");
+  }
+
+  // now you're good; define everything on load
+  window.addEventListener('load', function (event) {
+    __CS132__(window, window.BIEBER_URL, window.STUDENT_LOGIN, (void(0)));
+  }, false);
 
 }(window));
